1ab0e52df13d8b7c6eee561030e4994a05623d2b
Blake Mercer <blake.mercer93@gmail.com>
2020-11-23 12:53:37 -0500

    demonstrated knowledge of core redux features

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import resultReducer from './store/reducers/result';
+import countReducer from './store/reducers/counter';
+
+const rootReducer = combineReducers({
+  ctr: countReducer,
+  res: resultReducer,
+});
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
